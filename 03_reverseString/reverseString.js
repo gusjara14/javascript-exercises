6886bde5017c5bb57e74c78a3dd7c13001be160d
Gus <78346679+gusjara14@users.noreply.github.com>
2024-01-19 21:42:28 -0700

    Start exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let arrayLength = stringArray.length;
 
+    //next I need to push the array to another array in reverse order then join together and return
 };
 
 // Do not edit below this line
