dd9e85c2068660b3a1a3e84881128c0d14fc3d1d
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-01-20 14:37:10 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    let smallerInt;
+    let largerInt;
 
+    if(start<0 || end<0 || typeof start != "number" || typeof end != "number") return "ERROR";
+
+    if(start>end) {
+        largerInt = start;
+        smallerInt = end; 
+    } else {
+        largerInt = end;
+        smallerInt = start;
+    }
+    for(let index = smallerInt;index<=largerInt;index++) {
+        sum+=index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
