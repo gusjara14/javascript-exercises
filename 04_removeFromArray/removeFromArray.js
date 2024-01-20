e0d91290e9c46fe6ba12e06d7a582213654e20f7
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-01-20 14:03:08 -0700

    Complete first 2 tests for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...remove) {
+    console.log("Length of args: " + Object.keys(arguments).length);
 
+    
+    for(let argumentsIndex = 1;argumentsIndex<=Object.keys(arguments).length-1;argumentsIndex++) {
+        console.log(arguments[argumentsIndex]);
+        let removalIndex = array.indexOf(arguments[argumentsIndex]);
+        console.log("removalIndex: " + removalIndex);
+        array = array.slice(0, removalIndex).concat(array.slice(removalIndex+1));    
+
+    }
+   
+    return array;
 };
 
 // Do not edit below this line
